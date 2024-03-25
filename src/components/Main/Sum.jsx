03548047a35ca6e0186d34ac1ce0fdc32c7db078
leeyouna21 <yuna5845@gmail.com>
2024-03-25 22:44:55 +0900

    second

--- src/components/Main/Sum.jsx
+++ src/components/Main/Sum.jsx
@@ -4,7 +4,23 @@ const Summary = () => {
     const [isSumRightVisible, setIsSumRightVisible] = useState(true);
     const [newTask, setNewTask] = useState("");
     const [displayedTasks, setDisplayedTasks] = useState([]); // 입력값을 표시할 배열
+    const [TodoFocused, setTodoFocused] = useState(false); // 오른쪽 패널 투두리스트 클릭시 보더 나오는 부분
+    const [todoinputMsgPlaceholder, setTodoInputMsgPlaceholder] = useState("Click Here !!");
+    const [myLists, setMyLists] = useState([]);
 
+
+    const handleAddListEnter = (e) => {
+        if (e.key === "Enter") {
+            const newTodoItem = {
+                title: newTask,
+                complete: undefined
+            };
+            setMyLists((prevList) => [...prevList, newTodoItem]);
+            setNewTask(""); // 입력된 값 초기화
+        }
+    };
+    //끝
+    
     const handleInputText = (e) => {
         setNewTask(e.target.value); // 입력값 업데이트
     };
@@ -19,6 +35,45 @@ const Summary = () => {
         }
     };
 
+        //오른쪽 패널 투두리스트 add new task 입력된 값을 저장할 상태와 상태를 업데이트할 함수 시작
+        const handleInputChangeTodo = (event) => {
+            // input 값이 변경될 때마다 호출되는 함수
+            setNewTask(event.target.value); // 입력된 값으로 상태 업데이트
+        };
+        const handleInputFocus = () => {
+            setTodoInputMsgPlaceholder("Add New TodoList");
+        };
+        
+        const handleInputBlur = () => {
+            setTodoInputMsgPlaceholder("Click Here !!");
+        };
+    
+
+    //투두리스트 클릭시 border생성 함수 시작
+    const handleTodoFocused = () => {
+        setTodoFocused(true);
+    }
+
+    const handleTodoBlur = () => {
+        setTodoFocused(false);
+    }
+    //투두리스트 클릭시 border생성 함수 끝
+
+    // 창 열었다가 접기 기능
+    const [isScListVisible, setScListVisible] = useState({
+        scheduleList: true,
+        todoList: true,
+        completeList: true
+        });
+        
+        const handleFoldClick = (listName) => {
+        setScListVisible(prevState => ({
+            ...prevState,
+            [listName]: !prevState[listName]
+        }));
+        };
+    // 창 열었다가 접기 기능 끝
+
     const handleToggleSumRight = () => {
         setIsSumRightVisible(!isSumRightVisible);
     };
@@ -26,17 +81,23 @@ const Summary = () => {
         <div className="sum_wrap">
             <div className="sum_center">
                 <div className="center_container">
-                    <div className="mainBG">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="connection_wrap">
-                        <div className="conne">
-                            <h4></h4>
+                    <div className="center_left">
+                        <div className="mainBG">
+                            <img src="/mini2/public/image/text_img.png" alt="" className='changeBG'/>
+                        </div>
+                        <div className="connection_wrap">
+                            <div className="conne">
+                                <h4></h4>
+                            </div>
                         </div>
                     </div>
-                    <div className="sum">
-                        <h4></h4>
+                    
+                    <div className="center_right">
+                        <div className="sum">
+                            <h4>Summary!</h4>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
             {isSumRightVisible ? (
@@ -75,28 +136,42 @@ const Summary = () => {
                             <div className="title">
                                 <h4>Study planner</h4>
                             </div>
-                            <div className="study_wrap">
-                                <div className="add_list">
+                            {isScListVisible.todoList && (
+                            <div className="study_wrap" >
+                                <div className={`add_list ${TodoFocused ? 'focused' : ''}`} onClick={handleTodoFocused} onBlur={handleTodoBlur}>
                                     <div className="add_logo"></div>
-                                    <input 
-                                        placeholder='Add New Task' 
-                                        type="text" 
-                                        value={newTask}
-                                        onChange={handleInputText}
-                                        onKeyDown={handleKeyDown}
-                                    />
+                                    <h6>
+                                        <input 
+                                            type="text"
+                                            placeholder={todoinputMsgPlaceholder}
+                                            value={newTask}
+                                            onChange={handleInputChangeTodo}
+                                            onFocus={handleInputFocus}
+                                            onBlur={handleInputBlur}
+                                            onKeyPress={handleAddListEnter}
+                                        />
+                                    </h6>
                                 </div>
-                                <div className="chbox_wrap">
-                                    <div className="study_box">
-                                        {displayedTasks.map((task, index) => (
-                                            <div key={index}>
-                                                <input className='check' type='checkbox' />
-                                                <h5>{task}</h5>
+                                {/* 투두 함수를 받아와서 뿌려주어야 하는 위치 시작 */}
+                                <div className="my_listwrap">
+                                    {myLists.map((list, index) => list.complete == undefined &&  (
+                                        <div className="my_list" key={index}>
+                                            <div className="check_wrap">
+                                                <input
+                                                    type="checkbox"
+                                                />
                                             </div>
-                                        ))}
-                                    </div>
+                                            <div className="chbox_wrap">
+                                                <div className="study_box">
+                                                    <h5>{list.title}</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        )
+                                    )}
                                 </div>
                             </div>
+                        )}
                         </div>
                     </div>
                 </div>
